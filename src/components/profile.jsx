3aa7d95a9f801c8396a2e6812d1f4393a8ea73ce
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2025-04-18 05:36:36 +0100

    style profile page

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -2,9 +2,13 @@ import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase-config";
 import { collection, query, where, getDocs } from 'firebase/firestore';
-
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "../features/allPosts/allPostsSlice";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+
 
 function Profile(){
 
@@ -84,9 +88,48 @@ function Profile(){
             <div>
                 {posts?.map((post, index)=>{
                    (
-                    <div key={index} className="post">
-                        
-                    </div>
+                        <Link key={index} to={`/post/${post.title}`}>
+                            <div className="post">
+                                <div className="top-row">
+                                    {
+
+                                    post.authorImg? (
+                                        <figure>
+                                            <img src={post.authorImg} alt="" />
+                                        </figure>
+                                    ) : (
+                                        <div className="avatar">
+                                            {post.author?.charAt(0).toUpperCase()}
+                                        </div>
+                                    )}
+
+                                        <span className="name">{post.author}</span>
+                                        
+                                        <span className="date">{post.publishDate}</span>
+                                </div>
+
+                                <div className="post-info">
+                                        <div className="post-text">
+                                                <h2>{post.title}</h2>
+                                                <p>{post.content.slice(0,150)}</p>
+                                        </div>
+
+                                        {
+                                            post.image && 
+                                                <figure className="post-img">
+                                                    <img src="" alt=""/>
+                                                </figure>
+                                        }
+                                </div>
+
+                                <div className="bottom-row">
+                                            <span className= 'categ'><Link to={`category/${post.category}`}>{post.category}</Link></span>
+
+                                            <span><FontAwesomeIcon icon={faHeart} /> {post.likedBy.length} Likes</span>
+                                </div>
+
+                            </div>
+                    </Link>
                    ) 
                 })}
             </div>
